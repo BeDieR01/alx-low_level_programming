7ddf537322353538d07b3c5d9bb58ba7671eb391
BeDieR01 <bedierm5@gmail.com>
2023-07-13 14:15:19 -0700

    Auto C TASK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-/*main function is the entry of the code
+/*
+ * main function is the entry of the code
  *it will always return 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
