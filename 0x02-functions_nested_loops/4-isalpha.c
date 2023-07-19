8016530d30fc9d6620740fd885e2140a0cad4554
BeDieR01 <bedierm5@gmail.com>
2023-07-19 14:46:21 -0700

    Auto C Function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+
+	int _islower(int c)
+	{
+			if (c < 65 | c < 97 | c > 90 | c > 122)
+            {
+                return (0);
+            }
+            else 
+            {
+                return (1);
+            }
+	}
+
