6196163612ce4c6d64c42f7117b5cdbf71f72311
BeDieR01 <bedierm5@gmail.com>
2023-07-19 14:40:06 -0700

    Auto C Function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,19 +6,19 @@
 	 * followed by a new line
 	 */
 
-	void _islower(int c)
+	int _islower(int c)
 	{
 	    if (c < 97)
             {
-                _putchar('0');
+                return (0);
             }
             else if (c > 122)
             {
-                _putchar('0');
+                return (0);
             }
             else
             {
-                _putchar('1');
+                return (1);
             }
 	}
 
