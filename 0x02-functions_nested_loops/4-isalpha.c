3c7361c9ecb5aa3ee4028bc6c5027a62a665f682
BeDieR01 <bedierm5@gmail.com>
2023-07-19 14:49:58 -0700

    Auto C Function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,19 @@
 
 	int _islower(int c)
 	{
-			if ((c < 65) | (c < 97) | (c > 90) | (c > 122)
+			if (c < 65)
+            {
+                return (0);
+            }
+            else if (c < 97)
+            {
+                return (0);
+            }
+             else if (c > 90)
+            {
+                return (0);
+            }
+             else if (c > 122)
             {
                 return (0);
             }
