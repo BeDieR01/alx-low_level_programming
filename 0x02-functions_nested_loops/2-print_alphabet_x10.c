210a952f6d32c8243b0262c8fffc3d916c3d433a
BeDieR01 <bedierm5@gmail.com>
2023-07-19 14:26:02 -0700

    Auto C Function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,22 @@
 
 
 	/**
-	 * print_alphabet - print all alphabet in lowercase
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
 	 */
-
 
 	void print_alphabet_x10(void)
 	{
-		char letter;
-		int 1;
+			    char letter;
 
-        for (i = 0; i < 10; i++){
-		for (letter = 'a'; letter <= 'z'; letter++){
+        for (int i = 0; i < 10; i++)
+        {
+		for (letter = 'a'; letter <= 'z'; letter++)
+            {
 			_putchar(letter);
-		}
+            }
+            _putchar('\n');
         }
-
-		_putchar('\n');
+	    return (0);
 	}
 
