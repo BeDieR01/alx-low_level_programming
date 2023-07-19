6906a4856b3dd0c050a30a68d6a0cae68b27836e
BeDieR01 <bedierm5@gmail.com>
2023-07-19 15:17:51 -0700

    Auto C Function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+
+	void jack_bauer(void);
+	{
+			int i;
+			int j;
+			int m;
+			int n;
+			for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    for (m = 0; m < 6; m++)
+                    {
+                        for (n = 0; n < 10; n++)
+                        {
+                            _putchar(i);
+                            _putchar(j);
+                            _putchar(':');
+                            _putchar(m);
+                            _putchar(n);
+                            _putchar('\n');
+                        }
+                    }
+                }
+            }
+	}
+
