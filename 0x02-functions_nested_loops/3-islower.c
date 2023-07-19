3131d9800e5352bc685423d6ce0397bb18784e45
BeDieR01 <bedierm5@gmail.com>
2023-07-19 14:37:50 -0700

    Auto C Function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+
+	int _islower(int c)
+	{
+	    if (c < 97)
+            {
+                _putchar('0');
+            }
+            else if (c > 122)
+            {
+                _putchar('0');
+            }
+            else
+            {
+                _putchar('1');
+            }
+	}
+
